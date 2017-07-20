27025d2db943c3fcbbc030a417160965ce82ca91
Jason Kuchynka <jkuchynka@surgeforward.com>
2017-07-19 23:41:59 -0700

    Initial commit

---
+++ src/Dot.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Jbizzay;
+
+/**
+ * Helps manage large arrays and allows for accessing, setting,
+ * merging and unsetting with dot notation
+ */
+class Dot
+{
+
+    /**
+     * Data
+     *
+     * @var array
+     */
+    protected $data;
+
+    // @todo
+    protected $preserveNumericKeys = false;
+
+    /**
+     *
+     * @param array|null $data
+     */
+    public function __construct(array $data = null)
+    {
+        $this->data = $data ?: array();
+    }
+
+    /**
+     * Similar to get, but inits a value if not set
+     *
+     * @param string $key
+     * @param mixed $dataOrCallable Any data value or callable
+     * @return mixed Data
+     */
+    public function define(string $key, $dataOrCallable = array())
+    {
+        if ( ! $this->has($key)) {
+            if (is_callable($dataOrCallable)) {
+                $this->set($key, $dataOrCallable());
+            } else {
+                $this->set($key, $dataOrCallable);
+            }
+        }
+        return $this->get($key);
+    }
+
+    /**
+     * Get a value, returns null if not setf
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get(string $key = null)
+    {
+        if ( ! $key) {
+            return $this->data;
+        }
+        $target = $this->data;
+        $keys = explode('.', $key);
+        while ($key = array_shift($keys)) {
+            if ( ! isset($target[$key])) {
+                break;
+            }
+            if (empty($keys)) {
+                return $target[$key];
+            }
+            $target = &$target[$key];
+        }
+        return null;
+    }
+
+    /**
+     * Determine if key is set
+     *
+     * @param string $key
+     * @return boolean
+     */
+    public function has(string $key)
+    {
+        $target = $this->data;
+        $keys = explode('.', $key);
+        while ($key = array_shift($keys)) {
+            if (empty($keys)) {
+                if ( ! isset($target[$key])) {
+                    return false;
+                }
+                return true;
+            }
+            $target = &$target[$key];
+        }
+        return false;
+    }
+
+    /**
+     * Merge data recursively
+     *
+     * @param mixed $keyOrData
+     * @param mixed $dataOrCallable
+     * @param $this
+     */
+    public function merge($keyOrDataOrCallable, $dataOrCallable = null)
+    {
+        $keyOrData = is_callable($keyOrDataOrCallable) ? $keyOrDataOrCallable($this->data) : $keyOrDataOrCallable;
+        if (is_array($keyOrData)) {
+            $this->data = $this->mergeRecursive($this->data, $keyOrData);
+        } elseif (is_string($keyOrData)) {
+            $target = &$this->data;
+            $keys = explode('.', $keyOrData);
+            while ($key = array_shift($keys)) {
+                if ( ! isset($target[$key])) {
+                    $target[$key] = array();
+                }
+                if (empty($keys)) {
+                    $value = is_callable($dataOrCallable) ? $dataOrCallable($target[$key]) : $dataOrCallable;
+                    if (is_array($target[$key])) {
+                        $target[$key] = $this->mergeRecursive($target[$key], $value);
+                    } else {
+                        $target[$key] = $value;
+                    }
+                    break;
+                }
+                $target = &$target[$key];
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Set a value
+     *
+     * @param mixed $key
+     * @param mixed $dataOrCallable
+     * @param $this
+     */
+    public function set(string $key, $dataOrCallable = array())
+    {
+        $target = &$this->data;
+        $keys = explode('.', $key);
+        while ($key = array_shift($keys)) {
+            if ( ! isset($target[$key])) {
+                $target[$key] = array();
+            }
+            if (empty($keys)) {
+                if (is_callable($dataOrCallable)) {
+                    $target[$key] = $dataOrCallable($target[$key]);
+                } else {
+                    $target[$key] = $dataOrCallable;
+                }
+                break;
+            }
+            $target = &$target[$key];
+        }
+        return $this;
+    }
+
+    /**
+     * Unset a value
+     *
+     * @param string $key
+     * @return void
+     */
+    public function unset(string $key)
+    {
+        $target = &$this->data;
+        $keys = explode('.', $key);
+        while ($key = array_shift($keys)) {
+            if (empty($keys)) {
+                unset($target[$key]);
+            }
+            if (isset($target[$key])) {
+                $target = &$target[$key];
+            } else {
+                break;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Recursively merge 2 arrays
+     * On any level, if types differ, $from will replace $target
+     * If both are type other than array, $from will replace $target
+     * If both are array, string named key values will merge into $target,
+     * while any numbered keys will be appended to $target. In both cases,
+     * there will be no duplicates,
+     * and numbered keys in $from will not be preserved
+     *
+     * @param array $target
+     * @param mixed $from
+     * @return mixed $result
+     */
+    protected function mergeRecursive(array $target, $from)
+    {
+        if (is_callable($from)) {
+            $from = $from($target);
+        }
+        if (is_array($from)) {
+            foreach ($from as $key => $value) {
+                if (is_callable($value)) {
+                    $value = $value($target[$key]);
+                }
+                if (is_string($key)) {
+                    if (is_array($value)) {
+                        $target[$key] = $this->mergeRecursive($target[$key], $value);
+                    } else {
+                        $target[$key] = $value;
+                    }
+                } elseif ( ! in_array($value, $target)) {
+                    $target[] = $value;
+                }
+            }
+            return $target;
+        }
+        return $from;
+    }
+
+}
---
+++ tests/DotTest.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace Jbizzay\Tests;
+
+use Jbizzay\Dot;
+
+class DotTest extends \PHPUnit\Framework\TestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->dot = new Dot([
+            'a' => ['b', 'c', 'd'],
+            'b' => [
+                'a' => '1',
+                'b' => '2',
+                'c' => '3'
+            ],
+            'c' => [
+                'd' => [
+                    'e' => 'f'
+                ]
+            ],
+            'd' => [
+                'a' => 4,
+                'b' => 5.99
+            ]
+        ]);
+    }
+
+    public function testConstruct()
+    {
+        $dot = new Dot;
+        $this->assertEquals([], $dot->get());
+        $dot = new Dot(['foo' => 'bar']);
+        $this->assertEquals(['foo' => 'bar'], $dot->get());
+    }
+
+    public function testDefineAlreadySet()
+    {
+        $this->assertEquals(['b', 'c', 'd'], $this->dot->define('a', 'bar'));
+        $this->assertEquals('1', $this->dot->define('b.a', 'bar'));
+        $this->assertEquals('f', $this->dot->define('c.d.e', 'bar'));
+    }
+
+    public function testDefineDefaultsToArray()
+    {
+        $this->assertSame([], $this->dot->define('foo'));
+        $this->assertSame([], $this->dot->get('foo'));
+        $this->assertSame([], $this->dot->define('foo.bar'));
+        $this->assertSame([], $this->dot->get('foo.bar'));
+    }
+
+    public function testDefineValue()
+    {
+        $this->assertEquals('foo', $this->dot->define('newkey', 'foo'));
+        $this->assertEquals('foo', $this->dot->get('newkey'));
+        $this->assertEquals('bar', $this->dot->define('new.key.a', 'bar'));
+        $this->assertEquals('bar', $this->dot->get('new.key.a'));
+    }
+
+    public function testDefineLamda()
+    {
+        $multiply = function () {
+            return 8 * 8;
+        };
+
+        $this->assertEquals(64, $this->dot->define('e', $multiply));
+        $this->assertEquals(64, $this->dot->get('e'));
+        $this->assertEquals(64, $this->dot->define('c.d.f', $multiply));
+        $this->assertEquals(64, $this->dot->get('c.d.f'));
+    }
+
+    public function testGet()
+    {
+        $this->dot->set('key', 'val');
+        $this->assertEquals('val', $this->dot->get()['key']);
+        $this->assertSame(4, $this->dot->get('d.a'));
+        $this->assertSame(5.99, $this->dot->get('d.b'));
+        $this->assertEquals('f', $this->dot->get('c')['d']['e']);
+    }
+
+    public function testGetNotExistsReturnsNull()
+    {
+        $this->assertNull($this->dot->get('z.z.z.z.z'));
+    }
+
+    public function testHas()
+    {
+        $this->assertTrue($this->dot->has('a'));
+        $this->assertTrue($this->dot->has('c.d.e'));
+        $this->assertFalse($this->dot->has('z.z.z.z.z.z'));
+    }
+
+    public function testMergeArrayReplacesKey()
+    {
+        $this->assertEquals(['e' => 321, 'f' => 123], $this->dot->merge('c.d.e', ['e' => 321, 'f' => 123])->get('c.d.e'));
+    }
+
+    public function testMergeValueOverArray()
+    {
+        $this->assertEquals(123, $this->dot->merge(['c' => ['d' => 123]])->get('c.d'));
+    }
+
+    public function testMergeArrayWithArrayShouldntHaveDuplicates()
+    {
+        $this->assertEquals(['b', 'c', 'd', 'e'], $this->dot->merge('a', ['b', 'e'])->get('a'));
+    }
+
+    public function testMergeCallable()
+    {
+        $this->dot->merge(function () {
+            return [
+                'c' => [
+                    'd' => [
+                        'e' => 'z',
+                        'f' => 'g'
+                    ]
+                ]
+            ];
+        });
+        $this->assertEquals(['d' => ['e' => 'z', 'f' => 'g']], $this->dot->get('c'));
+    }
+
+    public function testMergeArrayWithArrayMixedTypes()
+    {
+        $this->dot->set('y.x.z', [
+            'a',
+            'z' => 123,
+            'c',
+            'f' => [
+                'foo'
+            ],
+            'e' => [
+                123 => 'test',
+                'hits' => 100
+            ]
+        ]);
+        $this->dot->merge([
+            'y' => [
+                'x' => [
+                    'z' => [
+                        'b',
+                        'z' => 'foo',
+                        'f' => [
+                            123 => 999
+                        ],
+                        'e' => function ($val) {
+                            $val['hits']++;
+                            return $val;
+                        }
+                    ]
+                ]
+            ]
+        ]);
+        $this->assertEquals([
+            'a',
+            'z' => 'foo',
+            'c',
+            'f' => [
+                'foo',
+                999
+            ],
+            'e' => [
+                123 => 'test',
+                'hits' => 101
+            ],
+            'b'
+        ], $this->dot->get('y.x.z'));
+        $this->assertEquals(4, $this->dot->get('d.a'));
+    }
+
+    public function testSetDefaultsToArray()
+    {
+        $this->assertSame([], $this->dot->set('x.y.z')->get('x')['y']['z']);
+    }
+
+    public function testSetValue()
+    {
+        $this->assertEquals('123', $this->dot->set('a', '123')->get('a'));
+        $this->assertSame(123, $this->dot->set('d.e.f.g', 123)->get('d.e.f.g'));
+    }
+
+    public function testSetCallable()
+    {
+        $this->dot->set('d.a', function ($value) {
+            return ++$value;
+        })->set('d.a', function ($value) {
+            return ++$value;
+        });
+        $this->assertSame(6, $this->dot->get('d.a'));
+    }
+
+    public function testUnset()
+    {
+        $this->assertNull($this->dot->unset('a')->get('a'));
+        $this->assertNull($this->dot->unset('c.d.e')->get('c.d.e'));
+    }
+
+    public function testUnsetNotExists()
+    {
+        $this->assertNull($this->dot->unset('z.z.z.z.z')->get('z.z.z.z.z'));
+    }
+
+}
